e1cde8d03f911a4c25b1281ba7458150a27ddc47
rongalis <rongali@live.in>
2022-11-23 14:48:05 -0600

    added new files

---
+++ ServiceKit/ServiceKit/Helpers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ServiceKit
+//
+//  Created by srikanth on 11/23/22.
+//
+
+import Foundation
+import UIKit
+
+public class FirstViewController: UIViewController{
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Print this - FirstViewController")
+    }
+    
+public func callThisFunction(){
+        print("Print this - callThisFunction")
+    }
+}
